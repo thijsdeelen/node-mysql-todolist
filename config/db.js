8bd9a229d3df9272e42a9a87c05fdab367488bbb
Robin Schellius <r.schellius@avans.nl>
2017-05-11 15:59:41 +0200

    Log feedback verbeterd

--- config/db.js
+++ config/db.js
@@ -6,20 +6,22 @@
 var mysql = require('mysql');
 var config = require('../config/config');
 
-var connection = mysql.createConnection({
-  host     : process.env.DB_HOST || config.dbHost,
-  user     : process.env.DB_USER || config.dbUser,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_DATABASE || config.dbDatabase
-});
+var connectionSettings = {
+    host: process.env.DB_HOST || config.dbHost,
+    user: process.env.DB_USER || config.dbUser,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE || config.dbDatabase
+}
+
+var connection = mysql.createConnection(connectionSettings);
 
 connection.connect(function(error) {
-	if (error) {
-		console.error("Error connecting to database " + config.dbDatabase + " on " + config.dbHost + ": " + error.message);
-		return;
-	} else {
-		console.log("Connected to MySQL database " + config.dbDatabase + " on " + config.dbHost);
-	}
+    if (error) {
+        console.error("Error connecting to database " + connectionSettings.database + " on " + connectionSettings.host + ": " + error.message);
+        return;
+    } else {
+        console.log("Connected to database " + connectionSettings.database + " on " + connectionSettings.host);
+    }
 });
 
 module.exports = connection;
