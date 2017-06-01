43a2a0e1703885ff4436d01fde54b4e52c3cec54
Robin Schellius <r.schellius@avans.nl>
2017-06-01 12:20:39 +0200

    Codecoverage toegevoegd.

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,14 @@
 var gulp = require('gulp');
 var sonarqubeScanner = require('sonarqube-scanner');
 var runSequence = require('run-sequence');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 
 //
 // Task om Sonar analyse te genereren.
 // Voer deze uit via 'gulp sonarqube' of via npm run sonar'.
 //
-gulp.task('sonarqube', function(callback) {
+gulp.task('sonarqube', ['test'], function(callback) {
     //
     // Info over instellingen van sonarqubeScanner: 
     // https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
@@ -29,10 +31,27 @@ gulp.task('sonarqube', function(callback) {
             "sonar.login": "9ff8119ecea9f6c15d6c461a62dcc2d8db3439e1",
             "sonar.projectName": "node-todolist",
             "sonar.working.directory": "./.sonar",
-            "sonar.exclusions": "gulpfile.js, .gitignore, *.md, *.yml, *.sql, *.txt, node_modules/**",
+            "sonar.tests": "test",
+            "sonar.javascript.lcov.reportPath": "coverage/lcov.info",
+            "sonar.exclusions": "gulpfile.js, .gitignore, *.md, *.yml, *.sql, *.txt, *.json, node_modules/**, coverage/**, test/**",
             "sonar.verbose": "true"
         }
     }, callback);
+});
+
+gulp.task('test', ['pre-test'], function() {
+    return gulp.src(['test/**/*.js'])
+        .pipe(mocha())
+        // Creating the reports after tests ran
+        .pipe(istanbul.writeReports());
+});
+
+gulp.task('pre-test', function() {
+    return gulp.src(['*.js', 'api/**/*.js', 'auth/**/*.js', 'config/**/*.js'])
+        // Covering files
+        .pipe(istanbul())
+        // Force `require` to return covered files
+        .pipe(istanbul.hookRequire());
 });
 
 //
